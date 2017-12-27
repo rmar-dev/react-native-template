e647aa3ab5820c2f2b073968c45bd1ab5c3220dd
Ricardo Rabeto <ricardorabeto@Ricardos-iMac.home>
2017-12-27 16:27:22 +0000

    start fresh project template

---
+++ __tests__/App.js
@@ -0,0 +1,9 @@
+import 'react-native';
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+import React from 'react';
+import App from '../App';
+
+it('renders correctly', () => {
+  const tree = renderer.create(<App />);
+});
